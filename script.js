1b63315f27925edc997c9bc2b7a25603332e39c1
ErnyDV <ernesto.delgadodv@gmail.com>
2025-03-26 15:35:29 -0600

    init

--- script.js
+++ script.js
@@ -8,17 +8,66 @@
 
 // Note: It is almost always best to return things rather than putting console.log() directly 
 // into the function. In this case, return the info string and log it after the function has been called:
+const myLibrary = []
 
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = crypto.randomUUID();
 
     this.info = function(){
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'already read' : 'not read yet'}`
     }
 }
 
-const book = new Book('Hogwarts', 'J.R.R', 200, false )
 
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read))
+}
+
+function addPadding(string){
+    let paddingNeeded = 40 - string.length
+    if (paddingNeeded % 2 === 0){
+        return `-` + ' '.repeat(paddingNeeded / 2 - 1) + string + ' '.repeat(paddingNeeded / 2 - 1) + '-'
+    }else {
+        return `-` + ' '.repeat(paddingNeeded / 2 - 1) + string + ' '.repeat(paddingNeeded / 2) + '-'
+    }
+}
+
+function displayBooks(library){
+
+    for(let book of library){
+        let card = `
+        ----------------------------------------
+        -                 Title                -
+        -                                      -
+        ${addPadding(book.title)}
+        ----------------------------------------
+        -                Author                -
+        -                                      -
+        ${addPadding(book.author)}
+        ----------------------------------------
+        -                Pages                 -
+        -                                      -
+        ${addPadding(String(book.pages))}
+        ----------------------------------------
+        -            Currently Read            -
+        -                                      -
+        ${addPadding(book.read ? 'Not read': 'Read')}
+        ----------------------------------------
+        -                  Id                  -
+        -                                      -
+        ${addPadding(book.id)}
+        ----------------------------------------
+        `
+        console.log(card)
+    }
+}
+
+addBookToLibrary('OPM', 'Saitama', 500, true);
+addBookToLibrary('Solo leveling', 'Wing Zao', 200, false);
+addBookToLibrary('Solo leveling', 'Wing Zao adfsdsd adsfasd', 200, false);
+
+displayBooks(myLibrary);
